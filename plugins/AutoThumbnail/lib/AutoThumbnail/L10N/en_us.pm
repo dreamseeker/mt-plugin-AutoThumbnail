294da2c34eda7ed3355860e8e8134ab7058f2112
TORU KOKUBUN <kokubun@d-s-b.jp>
2011-06-29 20:09:23 +0900

    language file

---
+++ plugins/AutoThumbnail/lib/AutoThumbnail/L10N/en_us.pm
@@ -0,0 +1,17 @@
+package AutoThumbnail::L10N::en_us;
+
+use strict;
+use base 'AutoThumbnail::L10N';
+use vars qw( %Lexicon );
+
+%Lexicon = (
+	'Description'				=>	'Thanks for your DL. This Plugin is Auto Thumbnail Image Generator.',
+	'Config Option1'			=>	'Thumbnail Image Size 1',
+	'Config Option2'			=>	'Thumbnail Image Size 2',
+	'Config Option3'			=>	'Thumbnail Image Size 3',
+	'Config Option4'			=>	'Suffix of Thumbnail File',
+	'Config Description Blog'	=>	'Resize Type(Width or Height), Maximam Size(Int), Forces Square(1 or 0)',
+	'Config Description System'	=>	'If you want to decide FileName Suffix. Enter Strings.',
+);
+
+1;
